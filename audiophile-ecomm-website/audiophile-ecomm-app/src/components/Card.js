519557be7782f15cb120fded3913512607ea1c8e
steve <steve.braco@gmail.com>
2021-05-08 01:32:03 +0200

    tablet

--- audiophile-ecomm-website/audiophile-ecomm-app/src/components/Card.js
+++ audiophile-ecomm-website/audiophile-ecomm-app/src/components/Card.js
@@ -9,13 +9,16 @@ export default function Card({ name, id, category, description, image, slug, img
               flexDirection: imgStart === 'start' ? 'row-reverse' : 'row'
             }}
     >
+      <picture className='card__picture'>
+      {/* <source media="(max-width: 985px)" srcset={image.tablet}/> */}
       <img className="card__img" src={image.desktop} alt={name} />
+      </picture>
       <div className="card__content">
         {newProduct ? (
           <p className="card__new-product l-spacing">new product</p>
         ) : null}
 
-        <h2 className="card__title">{name}</h2>
+        <h2 className="card__title ">{name}</h2>
         <p className="card__description black-op">{description}</p>
         <Link to={`/product/${id}`} className="btn btn-primary"> see product</Link>
       </div>
