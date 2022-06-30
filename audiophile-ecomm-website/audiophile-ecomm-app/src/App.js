e52904d0a46730c0d5e2d93de409329d87695156
steve <steve.braco@gmail.com>
2022-06-30 10:30:54 +0400

    delete file

--- audiophile-ecomm-website/audiophile-ecomm-app/src/App.js
+++ audiophile-ecomm-website/audiophile-ecomm-app/src/App.js
@@ -22,22 +22,20 @@ function App() {
   });
 
   return (
-    <>
-      <Router>
-        <ScrollToTop />
-        <Navbar />
-        <Switch>
-          <Route path="/" exact component={HomeScreen}></Route>
-          <Route path="/headphones" exact component={HeadPhonesScreen}></Route>
-          <Route path="/speakers" exact component={SpeakersScreen}></Route>
-          <Route path="/earphones" exact component={EarPhonesScreen}></Route>
-          <Route path="/product/:slug" component={ProductDetailScreen}></Route>
-          <Route path="/cart/:slug" component={CartScreen}></Route>
-          <Route path="/checkout" component={CheckoutScreen}></Route>
-        </Switch>
-        <Footer />
-      </Router>
-    </>
+    <Router>
+      <ScrollToTop />
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={HomeScreen}></Route>
+        <Route path="/headphones" exact component={HeadPhonesScreen}></Route>
+        <Route path="/speakers" exact component={SpeakersScreen}></Route>
+        <Route path="/earphones" exact component={EarPhonesScreen}></Route>
+        <Route path="/product/:slug" component={ProductDetailScreen}></Route>
+        <Route path="/cart/:slug" component={CartScreen}></Route>
+        <Route path="/checkout" component={CheckoutScreen}></Route>
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
